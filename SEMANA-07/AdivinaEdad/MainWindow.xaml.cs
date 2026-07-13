8fdbe96a748fac350a67c7445754df74f641a568
Algoritmo-arc <alexanderregaladocabreraarcmi@gmail.com>
2026-07-13 08:40:37 -0500

    Adivinar Edad Código Implementado

--- SEMANA-07/AdivinaEdad/MainWindow.xaml.cs
+++ SEMANA-07/AdivinaEdad/MainWindow.xaml.cs
@@ -1,24 +1,61 @@
-﻿using System.Text;
+﻿using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace AdivinaEdad
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private Random rnd = new Random();
+        private int contadorIntentos = 0;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnPrimerIntento_Click(object sender, RoutedEventArgs e)
+        {
+            contadorIntentos = 0;
+            RealizarIntento();
+
+            btnCorrecto.IsEnabled = true;
+            btnIncorrecto.IsEnabled = true;
+        }
+
+        private void btnIncorrecto_Click(object sender, RoutedEventArgs e)
+        {
+            RealizarIntento();
+        }
+
+        private void RealizarIntento()
+        {
+            if (int.TryParse(txtMinimo.Text, out int min) && int.TryParse(txtMaximo.Text, out int max))
+            {
+                if (min <= max)
+                {
+                    int edadGenerada = rnd.Next(min, max + 1);
+
+                    txtEdadAdivinada.Text = edadGenerada.ToString();
+                    contadorIntentos++;
+                }
+                else
+                {
+                    MessageBox.Show("El valor mínimo no puede ser mayor al máximo.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor, ingresa un intervalo de números válido.");
+            }
+        }
+
+        private void btnCorrecto_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Edad Correcta: {contadorIntentos} intentos");
+
+            txtEdadAdivinada.Text = "";
+            btnCorrecto.IsEnabled = false;
+            btnIncorrecto.IsEnabled = false;
         }
     }
 }
